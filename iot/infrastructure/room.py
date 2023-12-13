7604012aaab40a9f551e30d30e142b1e83705ea2
Roland Steinegger <roland@steinegger.one>
2023-12-13 11:29:34 +0100

    Adds temperature to room notifications

--- iot/infrastructure/room.py
+++ iot/infrastructure/room.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from iot.infrastructure.thing import Thing, _datetime_from_dict_key
-from iot.infrastructure.units import Temperature
+from iot.infrastructure.units import Temperature, from_dict as temperature_from_dict
 
 
 class Room(Thing):
@@ -16,11 +16,13 @@ class Room(Thing):
 
     def to_dict(self):
         return {"name": self.name,
+                "temperature": self.temperature.to_dict() if self.temperature else None,
                 "last_updated_at": self.last_updated_at.isoformat() if self.last_updated_at is not None else None,
                 "last_seen_at": self.last_seen_at.isoformat() if self.last_seen_at is not None else None}
 
 
 def from_dict(dictionary: dict):
-    return Room(dictionary['name'], dictionary['temperature'] if 'temperature' in dictionary else None,
+    return Room(dictionary['name'],
+                temperature=temperature_from_dict(dictionary['temperature']) if 'temperature' in dictionary else None,
                 last_updated_at=_datetime_from_dict_key(dictionary, 'last_updated_at'),
                 last_seen_at=_datetime_from_dict_key(dictionary, 'last_seen_at'))
