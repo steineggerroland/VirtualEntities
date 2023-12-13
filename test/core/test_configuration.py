e3541f3707667e2cc5dbd6059d4bff7fb575dcfb
Roland Steinegger <roland@steinegger.one>
2023-12-13 07:14:57 +0100

    Adds min room config

--- test/core/test_configuration.py
+++ test/core/test_configuration.py
@@ -30,18 +30,22 @@ class ConfigurationTest(unittest.TestCase):
 
     def test_min_config(self):
         min_config = configuration.load_configuration(DIR / "min_test_config.yaml")
-        self.assertEqual(min_config.mqtt.url, "mqtt.local")
+        self.assertEqual("mqtt.local", min_config.mqtt.url)
         self.assertIsNotNone(min_config.mqtt.port)
         self.assertFalse(min_config.mqtt.has_credentials)
         self.assertIsNotNone(min_config.mqtt.client_id)
 
-        self.assertEqual(min_config.things[0].name, "super_washer")
-        self.assertEqual(min_config.things[0].type, "washing_machine")
-        self.assertEqual(min_config.things[0].sources.consumption_topic, "washer/consumption/topic")
+        self.assertEqual("super_washer", min_config.things[0].name)
+        self.assertEqual("washing_machine", min_config.things[0].type)
+        self.assertEqual("washer/consumption/topic", min_config.things[0].sources.consumption_topic)
         self.assertIsNone(min_config.things[0].sources.loading_topic)
         self.assertIsNone(min_config.things[0].sources.unloading_topic)
+        self.assertFalse(min_config.things[0].destinations.planned_notifications)
 
-        self.assertTrue(len(min_config.things[0].destinations.planned_notifications) == 0)
+        self.assertEqual("Dining room", min_config.things[1].name)
+        self.assertEqual("room", min_config.things[1].type)
+        self.assertFalse(min_config.things[1].sources.list)
+        self.assertFalse(min_config.things[1].destinations.planned_notifications)
 
     def test_incomplete_sources_produce_errors(self):
         self.assertRaises(IncompleteConfiguration,
