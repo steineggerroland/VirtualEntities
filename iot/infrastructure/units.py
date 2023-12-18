fd47811ee95ce1992920a50a863b7ab45e5c89fd
Roland Steinegger <roland@steinegger.one>
2023-12-18 15:22:44 +0100

    Adds temperature and humidity thresholds

--- iot/infrastructure/units.py
+++ iot/infrastructure/units.py
@@ -21,3 +21,80 @@ class Temperature:
 
 def from_dict(dictionary: dict):
     return Temperature(value=dictionary['value'], unit=dictionary['unit'])
+
+
+class Range:
+    def __init__(self, lower_value: float, upper_value: float):
+        self.lower_value = lower_value
+        self.upper_value = upper_value
+
+    def __eq__(self, other):
+        if not isinstance(other, Range):
+            return False
+        return self.lower_value == other.lower_value and self.upper_value == other.upper_value
+
+
+class TemperatureRating(str, Enum):
+    UNKNOWN = "unknown"
+    CRITICAL_COLD = "critical_cold"
+    COLD = "cold"
+    OPTIMAL = "optimal"
+    HOT = "hot"
+    CRITICAL_HOT = "critical_hot"
+
+
+class TemperatureThresholds:
+    def __init__(self, optimal: Range, frostiness_threshold: float, heat_threshold: float):
+        self.optimal = optimal
+        self.frostiness_threshold = frostiness_threshold
+        self.heat_threshold = heat_threshold
+
+    def __eq__(self, other):
+        if not isinstance(other, TemperatureThresholds):
+            return False
+        return self.optimal == other.optimal and self.frostiness_threshold == other.frostiness_threshold and self.heat_threshold == other.heat_threshold
+
+    def to_dict(self) -> dict:
+        return {"optimal_lower": self.optimal.lower_value, "optimal_upper": self.optimal.upper_value,
+                "frostiness_threshold": self.frostiness_threshold, "heat_threshold": self.heat_threshold}
+
+
+def temperature_thresholds_from_dict(dictionary: dict) -> TemperatureThresholds | None:
+    if not dictionary:
+        return None
+    return TemperatureThresholds(optimal=Range(dictionary['optimal_lower'], dictionary['optimal_upper']),
+                                 frostiness_threshold=dictionary['frostiness_threshold'],
+                                 heat_threshold=dictionary['heat_threshold'])
+
+
+class HumidityRating(str, Enum):
+    UNKNOWN = "unknown"
+    CRITICAL_DRY = "critical_dry"
+    DRY = "dry"
+    OPTIMAL = "optimal"
+    WET = "wet"
+    CRITICAL_WET = "critical_wet"
+
+
+class HumidityThresholds:
+    def __init__(self, optimal: Range, drought_threshold: float, wetness_threshold: float):
+        self.optimal = optimal
+        self.drought_threshold = drought_threshold
+        self.wetness_threshold = wetness_threshold
+
+    def __eq__(self, other):
+        if not isinstance(other, HumidityThresholds):
+            return False
+        return self.optimal == other.optimal and self.drought_threshold == other.drought_threshold and self.wetness_threshold == other.wetness_threshold
+
+    def to_dict(self) -> dict:
+        return {"optimal_lower": self.optimal.lower_value, "optimal_upper": self.optimal.upper_value,
+                "drought_threshold": self.drought_threshold, "wetness_threshold": self.wetness_threshold}
+
+
+def humidity_thresholds_from_dict(dictionary: dict) -> HumidityThresholds | None:
+    if not dictionary:
+        return None
+    return HumidityThresholds(optimal=Range(dictionary['optimal_lower'], dictionary['optimal_upper']),
+                              drought_threshold=dictionary['drought_threshold'],
+                              wetness_threshold=dictionary['wetness_threshold'])
--- test/infrastructure/test_room.py
+++ test/infrastructure/test_room.py
@@ -6,7 +6,8 @@ from _datetime import timedelta
 
 from iot.infrastructure.room import Room, from_dict
 from iot.infrastructure.thing import OnlineStatus
-from iot.infrastructure.units import TemperatureUnit, Temperature
+from iot.infrastructure.units import TemperatureUnit, Temperature, TemperatureThresholds, Range, HumidityThresholds, \
+    TemperatureRating, HumidityRating
 
 
 class InitTest(unittest.TestCase):
@@ -26,6 +27,20 @@ class InitTest(unittest.TestCase):
         temperature = Temperature(value=14, unit=TemperatureUnit.DEGREE_CELSIUS)
         self.assertEqual(Room(name="room_name", temperature=temperature).temperature, temperature)
 
+    def test_humidity(self):
+        humidity = 66.21
+        self.assertEqual(Room(name="room_name", humidity=humidity).humidity, humidity)
+
+    def test_temperature_thresholds(self):
+        temperature_thresholds = TemperatureThresholds(Range(20, 23), 15, 30)
+        self.assertEqual(Room(name="room_name", temperature_thresholds=temperature_thresholds).temperature_thresholds,
+                         temperature_thresholds)
+
+    def test_humidity_thresholds(self):
+        humidity_thresholds = HumidityThresholds(Range(60, 70), 45, 85)
+        self.assertEqual(Room(name="room_name", humidity_thresholds=humidity_thresholds).humidity_thresholds,
+                         humidity_thresholds)
+
 
 class RoomTest(unittest.TestCase):
     def setUp(self):
@@ -43,17 +58,71 @@ class RoomTest(unittest.TestCase):
         self.assertAlmostEqual(self.room.last_updated_at, datetime.now(), delta=timedelta(seconds=1))
         self.assertAlmostEqual(self.room.last_seen_at, datetime.now(), delta=timedelta(seconds=1))
 
+    def test_rate_temperature(self):
+        temperature_thresholds = TemperatureThresholds(Range(20, 23), 15, 30)
+        self.assertEqual(
+            Room("r1", temperature=Temperature(10), temperature_thresholds=temperature_thresholds).rate_temperature(),
+            TemperatureRating.CRITICAL_COLD)
+        self.assertEqual(
+            Room("r2", temperature=Temperature(17), temperature_thresholds=temperature_thresholds).rate_temperature(),
+            TemperatureRating.COLD)
+        self.assertEqual(
+            Room("r3", temperature=Temperature(22), temperature_thresholds=temperature_thresholds).rate_temperature(),
+            TemperatureRating.OPTIMAL)
+        self.assertEqual(
+            Room("r4", temperature=Temperature(25), temperature_thresholds=temperature_thresholds).rate_temperature(),
+            TemperatureRating.HOT)
+        self.assertEqual(
+            Room("r5", temperature=Temperature(31), temperature_thresholds=temperature_thresholds).rate_temperature(),
+            TemperatureRating.CRITICAL_HOT)
+
+    def test_update_humidity(self):
+        self.room.humidity = 88.2
+        new_humidity = 66.42
+        # when
+        self.room.update_humidity(new_humidity)
+        # then
+        self.assertEqual(self.room.humidity, new_humidity)
+        self.assertAlmostEqual(self.room.last_updated_at, datetime.now(), delta=timedelta(seconds=1))
+        self.assertAlmostEqual(self.room.last_seen_at, datetime.now(), delta=timedelta(seconds=1))
+
+    def test_rate_humidity(self):
+        humidity_thresholds = HumidityThresholds(Range(60, 70), 45, 85)
+        self.assertEqual(Room("r1", humidity=40, humidity_thresholds=humidity_thresholds).rate_humidity(),
+                         HumidityRating.CRITICAL_DRY)
+        self.assertEqual(Room("r2", humidity=50, humidity_thresholds=humidity_thresholds).rate_humidity(),
+                         HumidityRating.DRY)
+        self.assertEqual(Room("r3", humidity=60, humidity_thresholds=humidity_thresholds).rate_humidity(),
+                         HumidityRating.OPTIMAL)
+        self.assertEqual(Room("r4", humidity=75, humidity_thresholds=humidity_thresholds).rate_humidity(),
+                         HumidityRating.WET)
+        self.assertEqual(Room("r5", humidity=90, humidity_thresholds=humidity_thresholds).rate_humidity(),
+                         HumidityRating.CRITICAL_WET)
+
     def test_to_dict_contains_all_information(self):
         name = 'Hallway 42'
         temperature = 42.1337
         humidity = 69.12
+        temperature_thresholds = TemperatureThresholds(Range(20, 23), 15, 30)
+        humidity_thresholds = HumidityThresholds(Range(60, 70), 45, 85)
         last_updated_at = datetime.now() - timedelta(minutes=2)
         last_seen_at = datetime.now() - timedelta(minutes=4)
         # when
-        room = Room(name, Temperature(temperature), humidity, last_updated_at, last_seen_at)
+        room = Room(name, Temperature(temperature), humidity, temperature_thresholds, humidity_thresholds,
+                    last_updated_at, last_seen_at)
         # then
         self.assertEqual(room.to_dict(), {"name": name, "temperature": {"value": temperature, "unit": "degree_celsius"},
-                                          "humidity": humidity, "online_status": OnlineStatus.ONLINE,
+                                          "humidity": humidity, 'temperature_thresholds': {'frostiness_threshold': 15,
+                                                                                           'heat_threshold': 30,
+                                                                                           'optimal_lower': 20,
+                                                                                           'optimal_upper': 23},
+                                          "temperature_rating": TemperatureRating.CRITICAL_HOT,
+                                          'humidity_thresholds': {'drought_threshold': 45,
+                                                                  'optimal_lower': 60,
+                                                                  'optimal_upper': 70,
+                                                                  'wetness_threshold': 85},
+                                          "humidity_rating": HumidityRating.OPTIMAL,
+                                          "online_status": OnlineStatus.ONLINE,
                                           "last_updated_at": last_updated_at.isoformat(),
                                           "last_seen_at": last_seen_at.isoformat()})
 
@@ -61,14 +130,20 @@ class RoomTest(unittest.TestCase):
         name = 'Hallway 42'
         temperature = Temperature(42.1337)
         humidity = 77.12
+        temperature_thresholds = TemperatureThresholds(Range(20, 23), 15, 30)
+        humidity_thresholds = HumidityThresholds(Range(60, 70), 45, 85)
         last_updated_at = datetime.now() - timedelta(minutes=2)
         last_seen_at = datetime.now() - timedelta(minutes=4)
         # when
-        reconstructed_room = from_dict(Room(name, temperature, humidity, last_updated_at, last_seen_at).to_dict())
+        reconstructed_room = from_dict(
+            Room(name, temperature, humidity, temperature_thresholds, humidity_thresholds,
+                 last_updated_at, last_seen_at).to_dict())
         # then
         self.assertEqual(name, reconstructed_room.name)
         self.assertEqual(temperature, reconstructed_room.temperature)
         self.assertEqual(humidity, reconstructed_room.humidity)
+        self.assertEqual(temperature_thresholds, reconstructed_room.temperature_thresholds)
+        self.assertEqual(humidity_thresholds, reconstructed_room.humidity_thresholds)
         self.assertEqual(last_updated_at, reconstructed_room.last_updated_at)
         self.assertEqual(last_seen_at, reconstructed_room.last_seen_at)
 
