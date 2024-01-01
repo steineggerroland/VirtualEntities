43595bda5c7dd6cd3291b3b110619033e79dea95
Roland Steinegger <roland@steinegger.one>
2024-01-01 12:04:03 +0100

    Refactors tests

--- test/infrastructure/machine/test_run_complete_strategy.py
+++ test/infrastructure/machine/test_run_complete_strategy.py
@@ -23,29 +23,34 @@ class TestSimpleHistoryRunCompleteStrategy(unittest.TestCase):
         self.assertTrue(self.strategy.is_run_completed(Mock()))
 
     def test_run_not_completed_when_history_contains_consumption_above_threshold(self):
+        below_threshold_count = 6
+        measurements_one_above_threshold = [self.generate_measurement(is_below_threshold=True) for _ in
+                                            range(below_threshold_count)]
+        measurements_one_above_threshold.insert(random.randint(0, below_threshold_count),
+                                                self.generate_measurement(False))
         # given
-        self.storage_mock.get_power_consumptions_for_last_seconds = Mock(
-            return_value=[self.generate_measurement(is_below_threshold=True),
-                          self.generate_measurement(is_below_threshold=False),
-                          self.generate_measurement(is_below_threshold=True),
-                          self.generate_measurement(is_below_threshold=True)])
+        self.storage_mock.get_power_consumptions_for_last_seconds = Mock(return_value=measurements_one_above_threshold)
         # when
         self.assertFalse(self.strategy.is_run_completed(Mock()))
 
     def test_run_completed_when_history_contains_only_below_threshold(self):
         # given
-        self.storage_mock.get_power_consumptions_for_last_seconds = Mock(
-            return_value=[self.generate_measurement(is_below_threshold=True),
-                          self.generate_measurement(is_below_threshold=True),
-                          self.generate_measurement(is_below_threshold=True),
-                          self.generate_measurement(is_below_threshold=True)])
+        measurements_below_threshold = [self.generate_measurement(is_below_threshold=True) for _ in range(6)]
+        self.storage_mock.get_power_consumptions_for_last_seconds = Mock(return_value=measurements_below_threshold)
         # when
         self.assertTrue(self.strategy.is_run_completed(Mock()))
 
     def generate_measurement(self, is_below_threshold=True) -> ConsumptionMeasurement:
         time = datetime.now() - timedelta(seconds=random.randint(0, self.duration_to_be_blow_threshold - 1))
-        return ConsumptionMeasurement(time, (random.random() * (self.threshold - .01)) if is_below_threshold else (
-                self.threshold + .01 + random.random() * 2000))
+        return ConsumptionMeasurement(time,
+                                      _below_threshold(self.threshold) if is_below_threshold else _above_threshold(
+                                          self.threshold))
+
+
+def _below_threshold(threshold) -> float: return random.random() * (threshold - .01)
+
+
+def _above_threshold(threshold) -> float: return threshold + .01 + random.random() * 2000
 
 
 if __name__ == '__main__':
