f1dfb0d177f6df9ae6b3ee60804343e14f150f1d
Roland Steinegger <roland@steinegger.one>
2024-01-03 11:18:44 +0100

    Adds json examples of updates sent to mqtt

--- test/mqtt/test_mqtt_room_mediator.py
+++ test/mqtt/test_mqtt_room_mediator.py
@@ -1,15 +1,29 @@
+import json
 import unittest
 from datetime import datetime, timedelta
+from pathlib import Path
 from unittest.mock import Mock, ANY, patch, call
 
 from waiting import wait
 
 from iot.core.configuration import IotThingConfig, Sources, Source, Destinations, PlannedNotification, Measure
 from iot.infrastructure.exceptions import DatabaseException
+from iot.infrastructure.room import Room
 from iot.infrastructure.room_service import RoomService
-from iot.infrastructure.units import Temperature
+from iot.infrastructure.units import Temperature, HumidityThresholds, Range, TemperatureThresholds
 from iot.mqtt.mqtt_client import MqttClient
 from iot.mqtt.mqtt_room_mediator import MqttRoomMediator
+
+DIR = Path(__file__).parent
+
+
+def _set_up_croniter():
+    # cron is replaced to announce two immediate responses and one in a week
+    patcher = patch('iot.mqtt.mqtt_mediator.croniter')
+    croniter_mock = patcher.start()
+    croniter = Mock()
+    croniter.get_next = Mock(side_effect=[datetime.now(), datetime.now(), datetime.now() + timedelta(weeks=1)])
+    croniter_mock.return_value = croniter
 
 
 class TemperatureTest(unittest.TestCase):
@@ -23,7 +37,6 @@ class TemperatureTest(unittest.TestCase):
                                       sources=Sources(
                                           [Source(topic="some/topic",
                                                   measures=[Measure(source_type='temperature', path="$")])]))
-        self.mqtt_client_mock.subscribe = Mock()
         # when
         MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, thing_config)
         # then
@@ -33,7 +46,6 @@ class TemperatureTest(unittest.TestCase):
         thing_config = IotThingConfig("Kitchen", "room",
                                       sources=Sources([Source(topic="some/topic", measures=[
                                           Measure(source_type='temperature', path="$.temperature")])]))
-        self.mqtt_client_mock.subscribe = Mock()
         MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, thing_config)
         mqtt_callback = self.mqtt_client_mock.subscribe.call_args[0][1]
         self.room_service_mock.update_temperature = Mock()
@@ -51,7 +63,6 @@ class TemperatureTest(unittest.TestCase):
         thing_config = IotThingConfig("Kitchen", "room",
                                       sources=Sources([Source(topic="some/topic", measures=[
                                           Measure(source_type='temperature')])]))
-        self.mqtt_client_mock.subscribe = Mock()
         MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, thing_config)
         mqtt_callback = self.mqtt_client_mock.subscribe.call_args[0][1]
         self.room_service_mock.update_temperature = Mock()
@@ -70,7 +81,6 @@ class TemperatureTest(unittest.TestCase):
                                           [Source(topic="some/topic", measures=[
                                               Measure(source_type='temperature',
                                                       path=unsupported_json_path)])]))
-        self.mqtt_client_mock.subscribe = Mock()
         MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, thing_config)
         mqtt_callback = self.mqtt_client_mock.subscribe.call_args[0][1]
         self.room_service_mock.update_temperature = Mock()
@@ -87,7 +97,6 @@ class TemperatureTest(unittest.TestCase):
                                           [Source(topic="some/topic", measures=[
                                               Measure(source_type='temperature',
                                                       path=unsupported_json_path)])]))
-        self.mqtt_client_mock.subscribe = Mock()
         MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, thing_config)
         mqtt_callback = self.mqtt_client_mock.subscribe.call_args[0][1]
         self.room_service_mock.update_temperature = Mock()
@@ -102,19 +111,38 @@ class TemperatureTest(unittest.TestCase):
         destinations = Destinations([PlannedNotification("some/topic", "* * * * * *")])
         mqtt_room_mediator = MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock,
                                               thing_config=IotThingConfig("Kitchen", "room", destinations=destinations))
-        self.mqtt_client_mock.publish = Mock()
-        # cron is replaced to announce two immediate responses and one in a week
-        patcher = patch('iot.mqtt.mqtt_mediator.croniter')
-        croniter_mock = patcher.start()
-        croniter = Mock()
-        croniter.get_next = Mock(side_effect=[datetime.now(), datetime.now(), datetime.now() + timedelta(weeks=1)])
-        croniter_mock.return_value = croniter
+        _set_up_croniter()
         # when
         mqtt_room_mediator.start()
         # then
         wait(lambda: sum(publish_args == call("some/topic", ANY) for publish_args in
                          self.mqtt_client_mock.publish.call_args_list) >= 2, timeout_seconds=1,
              waiting_for="mqtt.publish called several times")
+
+    def test_update_format_when_publishing(self):
+        # given
+        destinations = Destinations([PlannedNotification("some/topic", "* * * * * *")])
+        mqtt_room_mediator = MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock,
+                                              thing_config=IotThingConfig("Kitchen", "room", destinations=destinations))
+        self._set_up_room_matching_json()
+        _set_up_croniter()
+        # when
+        mqtt_room_mediator.start()
+        # then
+        with Path.open(DIR / "room-update.json") as file:
+            expected_json = json.loads(file.read())
+            # enums have to be converted
+            wait(lambda: sum(publish_args == call(ANY, ANY) for publish_args in
+                             self.mqtt_client_mock.publish.call_args_list) >= 1, timeout_seconds=1,
+                 waiting_for="mqtt.publish called several times")
+            self.assertDictEqual(expected_json, self.mqtt_client_mock.publish.call_args_list[0].args[1])
+
+    def _set_up_room_matching_json(self):
+        self.room_service_mock.room = Room("kitchen", Temperature(13.37), 42.42,
+                                           TemperatureThresholds(Range(18, 22), 16, 30),
+                                           HumidityThresholds(Range(50, 60), 40, 80),
+                                           datetime.fromisoformat("2024-01-02T03:04:05.678910"),
+                                           datetime.fromisoformat("2024-01-01T01:01:01.111111"))
 
     def test_handles_database_exceptions_without_breaking(self):
         mqtt_mediator = MqttRoomMediator(self.mqtt_client_mock, self.room_service_mock, IotThingConfig())
