0007142dabd73077bb8ec1a2e2a5d4bdd3c1f0aa
Roland Steinegger <roland@steinegger.one>
2024-01-03 23:19:01 +0100

    Introduces running state for iot machines in addition to its power state

--- iot/infrastructure/machine/dryer.py
+++ iot/infrastructure/machine/dryer.py
@@ -1,5 +1,5 @@
-from iot.infrastructure.thing import _datetime_from_dict_key
 from iot.infrastructure.machine.machine_that_can_be_loaded import MachineThatCanBeLoaded
+from iot.infrastructure.thing import _datetime_from_dict_key
 
 
 class Dryer(MachineThatCanBeLoaded):
@@ -12,5 +12,6 @@ def from_dict(dictionary: dict):
                  dictionary['needs_unloading'] if 'needs_unloading' in dictionary else False,
                  dictionary['is_loaded'] if 'is_loaded' in dictionary else False,
                  _datetime_from_dict_key(dictionary, 'started_run_at'),
+                 dictionary['running_state'] if 'running_state' in dictionary else None,
                  _datetime_from_dict_key(dictionary, 'finished_last_run_at'),
                  _datetime_from_dict_key(dictionary, 'last_seen_at'))
--- iot/infrastructure/machine/machine_that_can_be_loaded.py
+++ iot/infrastructure/machine/machine_that_can_be_loaded.py
@@ -1,17 +1,25 @@
 from datetime import datetime
+from enum import Enum
 
 from iot.infrastructure.machine.iot_machine import IotMachine
 
 
+class RunningState(str, Enum):
+    UNKNOWN = 'unknown'
+    IDLE = 'idle'
+    RUNNING = 'running'
+
+
 class MachineThatCanBeLoaded(IotMachine):
     def __init__(self, name, watt: float | None = None, last_updated_at: datetime | None = None, needs_unloading=False,
-                 is_loaded=False, started_last_run_at=None, finished_last_run_at=None,
+                 is_loaded=False, started_last_run_at=None, running_state=None,
+                 finished_last_run_at=None,
                  last_seen_at: None | datetime = None):
-        super().__init__(name, watt, last_updated_at=last_updated_at, last_seen_at=last_seen_at)
+        super().__init__(name, watt, last_updated_at=last_updated_at, last_seen_at=last_seen_at,
+                         started_last_run_at=started_last_run_at, finished_last_run_at=finished_last_run_at)
         self.needs_unloading = needs_unloading
         self.is_loaded = is_loaded or needs_unloading
-        self.started_run_at: datetime | None = started_last_run_at
-        self.finished_last_run_at: datetime | None = finished_last_run_at
+        self.running_state = running_state if running_state else RunningState.UNKNOWN
 
     def unload(self):
         self.needs_unloading = False
@@ -24,19 +32,19 @@ class MachineThatCanBeLoaded(IotMachine):
         self.last_updated_at = datetime.now()
 
     def start_run(self):
+        super().start_run()
         self.is_loaded = True
         self.needs_unloading = False
-        self.started_run_at = datetime.now()
-        self.last_updated_at = datetime.now()
+        self.running_state = RunningState.RUNNING
 
     def finish_run(self):
+        super().finish_run()
         if self.is_loaded:
             self.needs_unloading = True
-        self.started_run_at = None
-        self.finished_last_run_at = datetime.now()
-        self.last_updated_at = datetime.now()
+        self.running_state = RunningState.IDLE
 
     def to_dict(self):
         return super().to_dict() | {"is_loaded": self.is_loaded, "needs_unloading": self.needs_unloading,
                                     "started_run_at": self.started_run_at.isoformat() if self.started_run_at is not None else None,
+                                    "running_state": self.running_state,
                                     "finished_last_run_at": self.finished_last_run_at.isoformat() if self.finished_last_run_at is not None else None, }
