e06d49572df39022fa3a5c5b3114754416c8b13d
Roland H. Steinegger <roland@steinegger.one>
2024-05-26 21:28:07 +0200

    Implements enlisting persons

--- iot/infrastructure/register_of_persons.py
+++ iot/infrastructure/register_of_persons.py
@@ -3,4 +3,8 @@ class RegisterOfPersons:
         self._persons = []
 
     def enlist(self, person):
-        pass
+        matching_persons = list(filter(lambda p: p.name == person.name, self._persons))
+        if matching_persons:
+            self._persons.remove(matching_persons[0])
+        self._persons.append(person)
+        return True
