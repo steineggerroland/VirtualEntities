caf0c718244148f8902d6944f728041cf8f149aa
Roland H. Steinegger <roland@steinegger.one>
2024-05-26 21:34:31 +0200

    Tests locating persons in register

--- iot/infrastructure/register_of_persons.py
+++ iot/infrastructure/register_of_persons.py
@@ -1,17 +1,25 @@
+from typing import List
+
+from iot.infrastructure.person import Person
+
+
 class RegisterOfPersons:
     def __init__(self):
-        self._persons = []
+        self._persons: List[Person] = []
 
-    def enlist(self, person):
+    def enlist(self, person: Person) -> bool:
         matching_persons = list(filter(lambda p: p.name == person.name, self._persons))
         if matching_persons:
             self._persons.remove(matching_persons[0])
         self._persons.append(person)
         return True
 
-    def dismiss(self, name: str):
+    def dismiss(self, name: str) -> bool:
         matching_persons = list(filter(lambda p: p.name == name, self._persons))
         if matching_persons:
             self._persons.remove(matching_persons[0])
             return True
         return False
+
+    def locate(self, name: str) -> Person | None:
+        pass
