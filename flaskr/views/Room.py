e1a329aed0e2aa16a00a8f645830b3552d0ae166
Roland Steinegger <roland@steinegger.one>
2024-06-13 13:41:00 +0200

    Adds room configuration page

--- flaskr/views/Room.py
+++ flaskr/views/Room.py
@@ -21,3 +21,16 @@ class Details(View):
         else:
             flash(gettext("No room found with name '%(name)s'", name=name))
             return redirect(url_for("ve_list"))
+
+
+class Configuration(View):
+    def __init__(self, room_catalog: RoomCatalog):
+        self.room_catalog = room_catalog
+
+    def dispatch_request(self, name: str):
+        room = self.room_catalog.find_room(name)
+        if room is not None:
+            return render_template("room_configuration.html", room=room)
+        else:
+            flash(gettext("No room found with name '%(name)s'", name=name))
+            return redirect(url_for("ve_list"))
