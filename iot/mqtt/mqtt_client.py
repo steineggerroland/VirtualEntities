5073480d90d365a325a5644a9bc6cc8b42c8cb09
Roland Steinegger <roland@steinegger.one>
2024-06-15 12:36:54 +0200

    Logs mqtt connection settings to debug before connecting

--- iot/mqtt/mqtt_client.py
+++ iot/mqtt/mqtt_client.py
@@ -39,17 +39,24 @@ class Subscriptions:
 
 class MqttClient:
     def __init__(self, mqtt_config: MqttConfiguration):
+        self.logger = logging.getLogger(self.__class__.__qualname__)
         self.mqtt_config = mqtt_config
 
         self.mqtt_client = paho_mqtt.Client(client_id=self.mqtt_config.client_id)
         if self.mqtt_config.has_credentials:
             self.mqtt_client.username_pw_set(self.mqtt_config.username, self.mqtt_config.password)
+            self.logger.debug(
+                'Connecting to mqtt server "%s:pwlen%s@%s:%s"' % (
+                    mqtt_config.username, len(mqtt_config.password), mqtt_config.url, mqtt_config.port))
+        else:
+            self.logger.debug(
+                'Connecting to mqtt server "%s:%s"' % (mqtt_config.url, mqtt_config.port))
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_message = self.on_message
+
         self.mqtt_client.connect(self.mqtt_config.url, self.mqtt_config.port)
         self.loop_thread: Thread = Thread(target=self._loop_forever, daemon=True)
         self.subscriptions = Subscriptions()
-        self.logger = logging.getLogger(self.__class__.__qualname__)
 
     def start(self):
         if not self.loop_thread.is_alive():
