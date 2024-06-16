cf8a047b8fb972c47974e6748fa9147992d5e561
Roland Steinegger <roland@steinegger.one>
2024-06-16 10:34:09 +0200

    Verifies virtual entities are displayed

--- DoYouBehave/main.py
+++ DoYouBehave/main.py
@@ -4,12 +4,14 @@ import re
 import sys
 
 import pytest
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from testcontainers.core.docker_client import DockerClient
 from testcontainers_python_influxdb.influxdb2 import InfluxDb2Container
 
 from test.container.BehaveAppContainer import BehaveAppContainer
 from test.container.MosquittoContainer import MosquittoContainer
+from test.pages.virtual_entity_page import VirtualEntityPage
 
 
 class TestWebsite:
@@ -52,14 +54,47 @@ class TestWebsite:
             self.browser.quit()
         finally:
             try:
+                behave_app_container.stop()
+            except:
+                pass
+            try:
                 mqtt_container.stop()
             except:
                 pass
             try:
-                behave_app_container.stop()
+                influxdb_container.stop()
             except:
                 pass
 
     def test_root_redirects_to_virtual_entities(self):
         self.browser.get(self.BASE_URL)
         assert re.compile('.+virtual-entities.*').match(self.browser.current_url)
+
+    def test_overview_shows_virtual_entities(self):
+        self.browser.get(VirtualEntityPage.url(self.BASE_URL))
+        ve_page = VirtualEntityPage(self.browser)
+        assert ve_page.shows_virtual_entities()
+
+    def test_overview_shows_appliances(self):
+        self.browser.get(VirtualEntityPage.url(self.BASE_URL))
+        ve_page = VirtualEntityPage(self.browser)
+        for appliance_name in ['washing-machine', 'dishwasher', 'dryer']:
+            assert any(
+                appliance_element.find_element(By.CLASS_NAME, 'name').text == appliance_name for appliance_element in
+                ve_page.appliances())
+
+    def test_overview_shows_rooms(self):
+        self.browser.get(VirtualEntityPage.url(self.BASE_URL))
+        ve_page = VirtualEntityPage(self.browser)
+        for appliance_name in ['Parents bedroom', 'Kitchen', 'Bathroom', 'Hallway', 'Living room']:
+            assert any(
+                appliance_element.find_element(By.CLASS_NAME, 'name').text == appliance_name for appliance_element in
+                ve_page.rooms())
+
+    def test_overview_shows_persons(self):
+        self.browser.get(VirtualEntityPage.url(self.BASE_URL))
+        ve_page = VirtualEntityPage(self.browser)
+        for appliance_name in ['Robin', 'Ash']:
+            assert any(
+                appliance_element.find_element(By.CLASS_NAME, 'name').text == appliance_name for appliance_element in
+                ve_page.persons())
