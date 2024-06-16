ff6c5afb85993f21635c8671b264aec4dd4a5eb3
Roland Steinegger <roland@steinegger.one>
2024-06-17 01:59:12 +0200

    Verify power consumption updates on mqtt message

--- DoYouBehave/features/steps/steps.py
+++ DoYouBehave/features/steps/steps.py
@@ -1,3 +1,5 @@
+import random
+
 from behave import *
 from selenium.webdriver.common.by import By
 
@@ -7,6 +9,12 @@ from features.pages.virtual_entity_page import VirtualEntityPage
 @given('the user goes to the {page_name} page')
 def load_page(context, page_name: str):
     context.webdriver.get(context.urls[page_name])
+
+
+@When('the power consumption of the {appliance_name} is updated')
+def send_power_consumption(context, appliance_name: str):
+    context.new_value = random.random() * 2400
+    context.appliances[appliance_name].send_power_consumption_update(context.new_value)
 
 
 @then('they are {redirect_or_on} the {page_name} page')
@@ -20,7 +28,7 @@ def rooms_in_catalog(context):
     assert context.webdriver.find_elements(By.CSS_SELECTOR, '.room-catalog .room')
 
 
-@then('they see appliances in the appliance deoot')
+@then('they see appliances in the appliance depot')
 def appliances_in_depot(context):
     assert context.webdriver.find_elements(By.CSS_SELECTOR, '.appliance-depot .appliance')
 
@@ -45,9 +53,22 @@ def appliance_is_shown(context, appliance_names: str):
     for appliance_name in appliance_names:
         assert any(a.find_element(By.CLASS_NAME, 'name').text == appliance_name for a in ve_page.appliances())
 
+
 @then('they see the persons {person_names}')
 def appliance_is_shown(context, person_names: str):
     ve_page = VirtualEntityPage(context.webdriver)
     person_names = map(lambda p: p.strip().replace('"', ''), person_names.split(","))
     for person_name in person_names:
         assert any(p.find_element(By.CLASS_NAME, 'name').text == person_name for p in ve_page.persons())
+
+
+@then('the user sees the new power consumption for the {appliance_name} after a refresh')
+def appliance_has_power_consumption(context, appliance_name):
+    context.webdriver.refresh()
+    found_matching_appliance = False
+    for appliance_element in context.webdriver.find_elements(By.CSS_SELECTOR, '.appliance'):
+        if appliance_element.find_element(By.CLASS_NAME, 'name').text == appliance_name:
+            watt_text = appliance_element.find_element(By.CLASS_NAME, 'power-consumption').text
+            assert float(watt_text.replace('W', '')) == float(context.new_value)
+            found_matching_appliance = True
+    assert found_matching_appliance
--- DoYouBehave/features/virtual_entities_overview.feature
+++ DoYouBehave/features/virtual_entities_overview.feature
@@ -3,7 +3,7 @@ Feature: Showing general information of virtual entities
   Scenario: All virtual entity types are present
     Given the user goes to the virtual entities page
     Then they see rooms in the room catalog
-    And they see appliances in the appliance deoot
+    And they see appliances in the appliance depot
     And they see person in the register of persons
 
   Scenario: The user sees all rooms
@@ -17,3 +17,8 @@ Feature: Showing general information of virtual entities
   Scenario: The user sees all persons
     Given the user goes to the virtual entities page
     Then they see the persons Robin, Ash
+
+  Scenario: Power consumption updates are displayed
+    Given the user goes to the virtual entities page
+    When the power consumption of the Dryer is updated
+    Then the user sees the new power consumption for the Dryer after a refresh
