4217baf09afc69d1e9d454c0af3663eb4d3367a6
Roland Steinegger <roland@steinegger.one>
2024-07-03 12:31:50 +0200

    Users can set needs unloading and unload appliances on the page

--- flaskr/api/ApplianceDepot.py
+++ flaskr/api/ApplianceDepot.py
@@ -1,26 +1,38 @@
 from http import HTTPStatus
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, make_response
 
 from iot.core.configuration_manager import ConfigurationManager
 from iot.core.time_series_storage import TimeSeriesStorage
 from iot.infrastructure.machine.appliance_depot import ApplianceDepot
+from iot.infrastructure.machine.machine_service import MachineService
 
 
-def appliance_depot_api(appliance_depot: ApplianceDepot, time_series_storage: TimeSeriesStorage):
+def appliance_depot_api(appliance_service: MachineService, appliance_depot: ApplianceDepot,
+                        time_series_storage: TimeSeriesStorage):
     api = Blueprint('appliance-depot_api', __name__)
 
     @api.get('/appliances')
-    def all():
+    def inventory():
         return list(map(lambda a: a.to_dict(), appliance_depot.inventory()))
 
     @api.get('/appliances/<name>')
-    def single(name: str):
+    def details(name: str):
         return appliance_depot.retrieve(name).to_dict()
 
     @api.get('/appliances/<name>/power-consumptions')
-    def single_power_consumptions(name: str):
+    def power_consumptions(name: str):
         return list(
             map(lambda c: c.to_dict(), time_series_storage.get_power_consumptions_for_last_seconds(60 * 60 * 4, name)))
+
+    @api.post('/appliances/<name>/unload')
+    def unload(name: str):
+        appliance_service.unloaded(name)
+        return '', HTTPStatus.OK
+
+    @api.post('/appliances/<name>/load')
+    def load(name: str):
+        appliance_service.loaded(name, True)
+        return '', HTTPStatus.OK
 
     return api
