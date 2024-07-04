15602b4070cead6596900aa74af34e17d9349c51
Roland Steinegger <roland@steinegger.one>
2024-07-04 09:22:50 +0200

    Fixes not drawing diagram when sensors update less often

--- flaskr/api/ApplianceDepot.py
+++ flaskr/api/ApplianceDepot.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timedelta
 from http import HTTPStatus
 
 from flask import Blueprint, Response, make_response
 
 from iot.core.configuration_manager import ConfigurationManager
 from iot.core.time_series_storage import TimeSeriesStorage
+from iot.core.timeseries_types import ConsumptionMeasurement
 from iot.infrastructure.machine.appliance_depot import ApplianceDepot
 from iot.infrastructure.machine.machine_service import MachineService
 
@@ -22,8 +24,16 @@ def appliance_depot_api(appliance_service: MachineService, appliance_depot: Appl
 
     @api.get('/appliances/<name>/power-consumptions')
     def power_consumptions(name: str):
+        power_consumptions = time_series_storage.get_power_consumptions_for_last_seconds(60 * 60 * 4, name)
+        appliance = appliance_depot.retrieve(name)
+        if appliance.watt is not None:
+            if len(power_consumptions) == 0:  # last update was before time interval
+                power_consumptions.append(
+                    ConsumptionMeasurement(max(datetime.now() - timedelta(seconds=60 * 60 * 4), appliance.last_seen_at),
+                                           appliance.watt))
+            power_consumptions.append(ConsumptionMeasurement(datetime.now(), appliance.watt))
         return list(
-            map(lambda c: c.to_dict(), time_series_storage.get_power_consumptions_for_last_seconds(60 * 60 * 4, name)))
+            map(lambda c: c.to_dict(), power_consumptions))
 
     @api.post('/appliances/<name>/unload')
     def unload(name: str):
