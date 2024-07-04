304ea4c0ac83c3ef2f268d0a2a0447ddb189c262
Roland Steinegger <roland@steinegger.one>
2024-07-04 09:22:50 +0200

    Fixes not drawing diagram when sensors update less often

--- flaskr/api/RoomCatalog.py
+++ flaskr/api/RoomCatalog.py
@@ -1,8 +1,10 @@
 import logging
+from datetime import datetime, timedelta
 
 from flask import Blueprint
 
 from iot.core.time_series_storage import TimeSeriesStorage
+from iot.core.timeseries_types import TemperatureHumidityMeasurement
 from iot.infrastructure.room_catalog import RoomCatalog
 
 
@@ -20,13 +22,26 @@ def room_catalog_api(room_catalog: RoomCatalog, time_series_storage: TimeSeriesS
 
     @api.get('/rooms/<name>/temperature')
     def temperature_time_line(name: str):
-        temperature_time_series = list(map(lambda c: c.to_dict(), time_series_storage.get_room_climate_for_last_seconds(60 * 60 * 4, name)))
-        logger.debug(f'{name}s` temperature: {temperature_time_series}')
-        return temperature_time_series
+        time_series = _get_time_series(name)
+        logger.debug(f'{name}s` temperature: {time_series}')
+        return list(map(lambda c: c.to_dict(), time_series))
 
     @api.get('/rooms/<name>/humidity')
     def humidity_time_line(name: str):
-        humidity_time_series = list(map(lambda c: c.to_dict(), time_series_storage.get_room_climate_for_last_seconds(60 * 60 * 4, name)))
-        logger.debug(f'{name}s` humidity: {humidity_time_series}')
-        return humidity_time_series
+        time_series = _get_time_series(name)
+        logger.debug(f'{name}s` humidity: {time_series}')
+        return list(map(lambda c: c.to_dict(), time_series))
+
+    def _get_time_series(name):
+        time_series = time_series_storage.get_room_climate_for_last_seconds(60 * 60 * 4, name)
+        room = room_catalog.find_room(name)
+        if room.humidity is not None and room.temperature is not None:
+            if len(time_series) == 0:  # last update was before time interval
+                time_series.append(
+                    TemperatureHumidityMeasurement(
+                        max(datetime.now() - timedelta(seconds=60 * 60 * 4), room.last_seen_at),
+                        room.temperature.value, room.humidity))
+            time_series.append(TemperatureHumidityMeasurement(datetime.now(), room.temperature.value, room.humidity))
+        return time_series
+
     return api
