412f966559aa80a8ab34c62be107224b78069ae9
Roland Steinegger <roland@steinegger.one>
2024-07-07 21:38:50 +0200

    Fixes possible error

--- flaskr/static/javascript/room-climate-diagrams.js
+++ flaskr/static/javascript/room-climate-diagrams.js
@@ -96,7 +96,7 @@
     function makeSafeForCSS(name) {
         return name.replace(/[^a-z0-9]/g, function (s) {
             let c = s.charCodeAt(0);
-            if (c == 32) return '-';
+            if (c === 32) return '-';
             if (c >= 65 && c <= 90) return '_' + s.toLowerCase();
             return '__' + ('000' + c.toString(16)).slice(-4);
         });
