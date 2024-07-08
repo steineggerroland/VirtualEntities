d1da6f902b554418bffaf9afb36b8837509588f4
Roland Steinegger <roland@steinegger.one>
2024-07-08 21:54:40 +0200

    Adds dashboard for appliances and rooms

--- flaskr/views/VirtualEntities.py
+++ flaskr/views/VirtualEntities.py
@@ -7,13 +7,26 @@ from iot.infrastructure.room_catalog import RoomCatalog
 
 
 class ListView(View):
-    def __init__(self, appliance_depot: ApplianceDepot, room_catalog: RoomCatalog, register_of_persons: RegisterOfPersons):
+    def __init__(self, appliance_depot: ApplianceDepot, room_catalog: RoomCatalog,
+                 register_of_persons: RegisterOfPersons):
         self.appliance_depot: ApplianceDepot = appliance_depot
         self.room_catalog: RoomCatalog = room_catalog
         self.register_of_persons: RegisterOfPersons = register_of_persons
+
     def dispatch_request(self):
         appliances = self.appliance_depot.inventory()
         rooms = self.room_catalog.list_all_rooms()
         persons = self.register_of_persons.catalog_all()
         return render_template("virtual_entities.html", entities={'appliance_depot': appliances, 'room_catalog': rooms,
                                                                   'register_of_persons': persons})
+
+
+class Dashboard(View):
+    def __init__(self, appliance_depot: ApplianceDepot, room_catalog: RoomCatalog):
+        self.appliance_depot: ApplianceDepot = appliance_depot
+        self.room_catalog: RoomCatalog = room_catalog
+
+    def dispatch_request(self):
+        appliances = self.appliance_depot.inventory()
+        rooms = self.room_catalog.list_all_rooms()
+        return render_template("dashboard.html", entities={'appliance_depot': appliances, 'room_catalog': rooms})
