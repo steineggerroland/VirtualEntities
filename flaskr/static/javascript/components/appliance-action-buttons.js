abace19dd0f7476ae97bfb3e8530036411ecc993
Roland Steinegger <roland@steinegger.one>
2024-07-16 20:17:23 +0200

    Does not need refresh when un-/loading appliance on website

---
+++ flaskr/static/javascript/components/appliance-action-buttons.js
@@ -0,0 +1,42 @@
+import {socket} from "../refresh.js";
+
+const ActionButtons = function () {
+    const self = this;
+    const socketHandler = event => {
+        const oldAppliance = JSON.parse(self.dataset.applianceJson)
+        self.dataset.applianceJson = JSON.stringify(event.appliance)
+        if (oldAppliance.running_state !== event.appliance.running_state ||
+            (oldAppliance.needs_unloading !== event.appliance.needs_unloading)) {
+            console.log(`${new Date().getTime()}: buttons refresh`)
+            socket.off(`appliances/${JSON.parse(self.dataset.applianceJson).name}/updated`, socketHandler);
+            self.refresh()
+        }
+    }
+    socket.on(`appliances/${JSON.parse(self.dataset.applianceJson).name}/updated`, socketHandler);
+    self.submitUnload = function (event) {
+        if (event.preventDefault) event.preventDefault();
+        fetch(self.dataset.unloadApiUrl, {method: "POST"})
+        return false;
+    }
+    self.submitLoad = function (event) {
+        if (event.preventDefault) event.preventDefault();
+        fetch(self.dataset.loadApiUrl, {method: "POST"})
+        return false;
+    }
+
+    self.unloadLabel = self.dataset.unloadLabel
+    self.loadLabel = self.dataset.loadLabel
+    const appliance = JSON.parse(self.dataset.applianceJson)
+    const unloadButtonHtml = `<form class="w-75 overflow-hidden" style="aspect-ratio: 1" method="get"
+                      action="#" onsubmit="self.submitUnload">
+                    <button class="unload btn btn-outline-success p-0 w-100 h-100"
+                            type="submit" value="{{ self.unloadLabel }}">{{ self.unloadLabel }}</button>
+                </form>`
+    const loadButtonHtml = `<form class="w-75 overflow-hidden" style="aspect-ratio: 1" method="get"
+                      action="#" onsubmit="self.submitLoad">
+                    <button class="needs-unloading btn btn-outline-primary p-0 w-100 h-100"
+                            type="submit" value="{{ self.loadLabel }}">{{ self.loadLabel }}</button>
+                </form>`
+    return !!appliance.needs_unloading ? unloadButtonHtml : (appliance.running_state !== 'running') ? loadButtonHtml : '<div></div>'
+}
+export {ActionButtons}
---
+++ flaskr/static/javascript/components/appliance-state.js
@@ -0,0 +1,101 @@
+import {socket} from "../refresh.js";
+
+const ApplianceState = function () {
+    const self = this;
+    const socketHandler = event => {
+        const oldAppliance = JSON.parse(self.dataset.applianceJson)
+        self.dataset.applianceJson = JSON.stringify(event.appliance)
+        if (oldAppliance.running_state !== event.appliance.running_state ||
+            (oldAppliance.type !== event.appliance.type) ||
+            (oldAppliance.needs_unloading !== event.appliance.needs_unloading) ||
+            (oldAppliance.started_run_at !== event.appliance.started_run_at)) {
+            if (self.interval) {
+                clearInterval(self.interval)
+            }
+            console.log(`${new Date().getTime()}: state refresh`)
+            socket.off(`appliances/${JSON.parse(self.dataset.applianceJson).name}/updated`, socketHandler);
+            self.refresh()
+        }
+    };
+    socket.on(`appliances/${JSON.parse(self.dataset.applianceJson).name}/updated`, socketHandler);
+    const appliance = JSON.parse(self.dataset.applianceJson)
+    self.text = self.color = self.icon = null
+    if (appliance.running_state === 'running') {
+        self.text = self.dataset.runningLabel
+        self.color = 'warning'
+        switch (appliance.type) {
+            case 'dishwasher':
+                self.icon = self.dataset.runningDishwasherIconUrl
+                break
+            case 'dryer':
+                self.icon = self.dataset.runningDryerIconUrl
+                break
+            case 'washing_machine':
+                self.icon = self.dataset.runningWashingMachineIconUrl
+                break
+            default:
+                self.icon = self.dataset.runningApplianceIconUrl
+        }
+        self.runTimer = ''
+        if (!!appliance.started_run_at) {
+            let startTime = new Date(appliance.started_run_at);
+
+            function updateElapsedTime() {
+                let currentTime = new Date();
+                let timeDifference = currentTime - startTime;
+
+                //let seconds = Math.floor((timeDifference / 1000) % 60);
+                let minutes = Math.floor((timeDifference / 1000 / 60) % 60);
+                let hours = Math.floor(timeDifference / 1000 / 60 / 60);
+
+                let formattedHours = ('' + hours).slice(-2);
+                let formattedMinutes = ('0' + minutes).slice(-2);
+                //let formattedSeconds = ('0' + seconds).slice(-2);
+
+                self.runTimer = `${formattedHours}:${formattedMinutes}`;
+                //timeSpan.textContent = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+            }
+
+            // Update time immediately
+            updateElapsedTime();
+            // Set the interval to update the time every second
+            self.interval = setInterval(updateElapsedTime, 30 * 1000);
+        }
+    } else if (appliance.needs_unloading) {
+        self.text = self.dataset.loadedLabel
+        self.color = 'danger'
+        switch (appliance.type) {
+            case 'dishwasher':
+                self.icon = self.dataset.loadedDishwasherIconUrl
+                break
+            case 'dryer':
+                self.icon = self.dataset.loadedDryerIconUrl
+                break
+            case 'washing_machine':
+                self.icon = self.dataset.loadedWashingMachineIconUrl
+                break
+            default:
+                self.icon = self.dataset.loadedApplianceIconUrl
+        }
+    } else {
+        self.text = self.dataset.idlingLabel
+        self.color = 'success'
+    }
+    self.runningSinceLabel = self.dataset.runningSinceLabel
+    let template = `<div class="running-state w-100 text-bg-{{ self.color }} bg-{{ self.color }} align-content-center text-center fs-4"
+             style="aspect-ratio: 1" role="status">`
+    if (appliance.running_state == 'running') {
+        template += `<div class="visually-hidden">{{self.text}}</div>
+        <img class="icon" src="{{self.icon}}" alt="{{self.runningSinceLabel}}"/>
+        <div>{{self.runTimer}}</div>`
+    } else if (appliance.needs_unloading) {
+        template += `<img class="icon" src="{{self.icon}}"
+             alt="{{self.finishedLastRunBeforeLabel}}"/>
+        <div>{{self.text}}</div>`
+    } else {
+        template += `{{self.text}}`
+    }
+    template += `</div>`
+    return template
+}
+export {ApplianceState}
