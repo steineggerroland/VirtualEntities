ba4f8b9d95f39617cf0aec61f121937528e5358c
Roland Steinegger <roland@steinegger.one>
2024-08-02 13:55:36 +0200

    Adds some appliance types

--- project.py
+++ project.py
@@ -1,7 +1,8 @@
 class Project:
     url = "https://github.com/steineggerroland/VirtualEntities"
     appliance_types_that_can_be_loaded = [
-        'washing_machine', 'dryer', 'dishwasher', 'coffee_machine', 'vacuum', 'top_loader'
+        'washing_machine', 'dryer', 'dishwasher', 'coffee_machine', 'vacuum', 'top_loader', 'freezer', 'fridge',
+        'refrigerator', 'fridge-freezer', 'chest_freezer'
     ]
     appliance_types_that_need_to_be_cleaned = [
 
