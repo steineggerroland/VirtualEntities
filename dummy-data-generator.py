2ca08652e00c3fe2eaca4fc37b34a7070ce2b30e
Roland Steinegger <roland@steinegger.one>
2024-08-09 19:11:00 +0200

    Reduces verbosity of generator and adds topics for all demo appliances

--- dummy-data-generator.py
+++ dummy-data-generator.py
@@ -2,19 +2,20 @@ import json
 import logging
 import sys
 import threading
-from random import random
+from random import random, choice
 from time import sleep
 
 import paho.mqtt.client as paho_mqtt
 
 from iot.core.configuration import MqttMeasureSource
 from iot.core.configuration_manager import ConfigurationManager
+from iot.infrastructure.appliance.appliance_builder import ApplianceBuilder
 
 CONFIG_FILE_NAME = sys.argv[1] if len(sys.argv) > 1 else 'config.yaml'
 continuous: bool = sys.argv.count('--continuous') > 0
 
 logging.basicConfig(encoding='utf-8',
-                    level=logging.DEBUG,
+                    level=logging.INFO,
                     format='%(asctime)s - %(name)s(%(lineno)s) - %(levelname)s - %(message)s')
 
 config = ConfigurationManager().load(CONFIG_FILE_NAME)
@@ -38,23 +39,36 @@ def notify_repeatedly():
 
 
 def send_random_data_for_entities():
+    count = 0
     for entity in config.entities:
-        if entity.type == "appliance":
+        if ApplianceBuilder.can_build(entity.type):
             if entity.sources:
                 for source in entity.sources.list:
-                    if type(source) is MqttMeasureSource:
+                    if type(source) is MqttMeasureSource and any(m.type == 'consumption' for m in source.measures):
                         source: MqttMeasureSource = source
-                        client.publish(source.mqtt_topic, round(random() * 2400, 2))
+                        rand = choice(['off', 'idle', 'running'])
+                        if rand == 'off':
+                            payload = 0
+                        elif rand == 'idle':
+                            payload = round(random() * 10, 2)
+                        else:
+                            payload = round(random() * 2400, 2)
+                        logging.debug('Sending to %s consumption %s', source.mqtt_topic, payload)
+                        client.publish(source.mqtt_topic, payload)
+                        count += 1
                         sleep(2)
         elif entity.type == "room":
             if entity.sources:
                 for source in entity.sources.list:
                     if type(source) is MqttMeasureSource:
                         source: MqttMeasureSource = source
-                        client.publish(source.mqtt_topic, json.dumps(
-                            {"temperature": 15 + round(random() * 20, 2), "humidity": round(random() * 100, 2)}))
+                        payload = json.dumps(
+                            {"temperature": 15 + round(random() * 20, 2), "humidity": round(random() * 100, 2)})
+                        logging.debug('Sending to %s indoor climate %s', source.mqtt_topic, payload)
+                        client.publish(source.mqtt_topic, payload)
+                        count += 1
                         sleep(2)
-    logging.debug('sent notifications')
+    logging.info('Sent %s notifications', count)
 
 
 if continuous:
